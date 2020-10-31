2efc7ba2941f0f7c455e39d48dafc775a89bf996
ashirvad46 <ashirvad46@gmail.com>
2020-10-31 18:20:58 +0530

    FE: Installed dependencies and configure i18n.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { withTranslation, Trans} from 'react-i18next'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedLang: "en"
+    }
+  }
+  onLanguageChange = (event) => {
+    let newLang = event.target.value;
+    this.setState({selectedLang: newLang})
+    this.props.i18n.changeLanguage(newLang)
+  }
+ 
+  renderRadioButtonLayout = () => {
+    return (
+      <div><input
+      checked={this.state.selectedLang === 'en'}
+      name="language" onChange={(e) => this.onLanguageChange(e)} value="en" type="radio" />English &nbsp; 
+      <input name="language" value="jap" 
+      checked={this.state.selectedLang === 'jap'}
+      type="radio" onChange={(e) => this.onLanguageChange(e)} />Japanese</div>
+    )
+  }
+
+  render(){
+    const {t, i18n} = this.props
+    return (
+      <>
+       <div style={{textAlign : 'center'}}>
+        <h3>{'Localisation'}</h3>
+        { this.renderRadioButtonLayout() }
+          <p>
+            {t('introduction')}
+          </p>
+          <Trans i18nKey="subtext" count={1} >
+            <p>I have translated the above line in <strong>1</strong>language.</p>
+           </Trans>
+       </div>
+        
+      </>  
+    );
+  }
 }
 
-export default App;
+export default withTranslation()(App);
